080e43bcf276288978503c4c17af21e1ac31d7ea
Ümit Tengiz <umitengiz@gmail.com>
2023-08-15 06:11:07 +0300

    Subfolder feature added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,33 +6,26 @@ import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+import { CMS_NAME, SUBFOLDER } from "../lib/constants";
 
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
   const morePosts = edges.slice(1)
 
   return (
-    <Layout preview={preview}>
-      <Head>
-        <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
-      </Head>
-      <Container>
-        <Intro />
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.featuredImage}
-            date={heroPost.date}
-            author={heroPost.author}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
-          />
-        )}
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Container>
-    </Layout>
-  )
+		<Layout preview={preview}>
+			<Head>
+				<title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+			</Head>
+			<Container>
+				<Intro />
+				{heroPost && (
+					<HeroPost title={heroPost.title} coverImage={heroPost.featuredImage} date={heroPost.date} author={heroPost.author} slug={heroPost.slug} subfolder={SUBFOLDER} excerpt={heroPost.excerpt} />
+				)}
+				{morePosts.length > 0 && <MoreStories posts={morePosts} />}
+			</Container>
+		</Layout>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
